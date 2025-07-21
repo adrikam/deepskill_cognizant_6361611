1875089a23bc04681ac6969ddde3728589180997
adrikam <2205872@kiit.ac.in>
2025-07-21 17:13:57 +0530

    Add files via upload

---
+++ week5/hands_on_kafka/KafkaConsumerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Confluent.Kafka;
+
+class KafkaConsumer
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "chat-consumer-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe("chat-topic");
+
+        Console.WriteLine("Listening for messages. Press Ctrl+C to exit.");
+
+        try
+        {
+            while (true)
+            {
+                var cr = consumer.Consume();
+                Console.WriteLine($"Received: {cr.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
---
+++ week5/hands_on_kafka/KafkaProducerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Confluent.Kafka;
+
+class KafkaProducer
+{
+    public static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        Console.WriteLine("Enter messages to send to Kafka (type 'exit' to quit):");
+
+        string? message;
+        while ((message = Console.ReadLine()) != "exit")
+        {
+            await producer.ProduceAsync("chat-topic", new Message<Null, string> { Value = message });
+            Console.WriteLine($"Sent: {message}");
+        }
+    }
+}
